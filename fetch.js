f8e0ddaea3724933c16c68b04f5517e228730201
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-02-23 02:11:45 +0600

    data Loaded

--- fetch.js
+++ fetch.js
@@ -13,6 +13,6 @@ function loadData() {
     const url = 'https://jsonplaceholder.typicode.com/todos/1';
     fetch(url)
         .then(response => response.json())
-        .then(response => console.log(json))
+        .then(data => console.log(data))
 
 }
---
+++ index.js
@@ -0,0 +1,18 @@
+function loadUser() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+
+        .then(res => res.json())
+        .then(data => displayUser(data))
+}
+
+function displayUser(data) {
+    const ul = document.getElementById('users-list');
+    for (let user of data) {
+        console.log(user.name);
+        const li = document.createElement('li');
+        li.innerText = user.name;
+
+        ul.appendChild(li);
+
+    }
+}
