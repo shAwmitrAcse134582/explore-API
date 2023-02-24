c3de8b9baa6654f9651946e91caae37c75787541
shAwmitrAcse134582 <shawmitra.cse.bu@gmail.com>
2023-02-24 11:23:54 +0600

    mealBD end

--- API-exmple/js/meal.js
+++ API-exmple/js/meal.js
@@ -14,7 +14,7 @@ const displaysMeals = meals => {
         const mealDiv = document.createElement('div')
         mealDiv.classList.add('col');
         mealDiv.innerHTML = `
-     <div class="card h-100">
+     <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
      <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
      <div class="card-body">
          <h5 class="card-title">${meal.strMeal}</h5>
@@ -34,4 +34,33 @@ const searchFood = () => {
     searchFeild.value = '';
 
 }
+
+
+
+const loadMealDetail = (idMeal) => {
+    // *console.log('get deteils of id', idMeal);
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    //* console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displaysMealDetails(data.meals[0]));
+}
+
+const displaysMealDetails = meal => {
+    const detailContainer = document.getElementById('detail-container');
+    detailContainer.innerHTML = ``;
+    const mealDiv = document.createElement('card');
+    mealDiv.classList.add('card');
+    mealDiv.innerHTML = `
+       <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${meal.strMeal}</h5>
+                    <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+                    <a href="#" class="btn btn-primary">Click here</a>
+                </div>
+    `;
+    detailContainer.appendChild(mealDiv);
+}
+
+loadMeal('');
 
